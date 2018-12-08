faad17b3a92d25152a57dc73204b6c368a11ff84
jozeff <your_email>
2018-12-08 14:08:23 -0800

    add workshop project to git

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    alert($("#css-item").text());
+})
+$(document).ready(function() {
+  $("#goal-button").click(function() {
+  $("#goal-list").append("<li class='list-item'>" + $("#new-goal").val() + "</li>");
+  $("#new-goal").val("");
+  })
+})
